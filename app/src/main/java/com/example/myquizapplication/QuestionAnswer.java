a46c8ea7100208d708fe1a6d5afdee587f8a3484
Anton12351 <cerepanovanton277@gmail.com>
2023-05-31 11:21:08 +0300

    cherepanov

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,20 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Какая компания владеет андроидом?",
+            "Кто первый полетел в Космос?",
+            "Кто создал 1 радио?"
+    };
+    public static String choices[][]={
+            {"Google","Apple","Nokia","Honor"},
+            {"Белка ", "Терешкова", "Гагарин", "Стрелка"},
+            {"Черепанов","Белкин","Карпова","Попов"}
+
+    };
+    public static String correctAnswers[]={
+            "Google",
+            "Гагарин",
+            "Попов"
+    };
+}
